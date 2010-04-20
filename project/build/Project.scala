5c8f39ee41d01c505d14cd2832348d40460c93c2
retronym <jzaugg@gmail.com>
2010-04-21 00:09:53 +0200

    Add JUnit test framework

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -1,19 +1,22 @@
 import sbt._
 
+trait Defaults {
+  this: DefaultProject =>
+  
+  val bryanjswift = "Bryan J Swift Repository" at "http://repos.bryanjswift.com/maven2/";
+  val junitInterface = "com.novocode" % "junit-interface" % "0.4.0"
+  
+  lazy val junit = "junit" % "junit" % "4.6"  
+}
 
 class Project(info: ProjectInfo) extends DefaultProject(info) {
 
-  lazy val common = project("common", "common", new DefaultProject(_) {
-    lazy val junit = "junit" % "junit" % "4.6"
-  })
+  lazy val common = project("common", "common", new DefaultProject(_) with Defaults)
   
-  def projectBasedOnCommon(nameAndFolder: String) = project(nameAndFolder, nameAndFolder, new DefaultProject(_) {
-    lazy val junit = "junit" % "junit" % "4.6"
-  }, common)
+  def projectBasedOnCommon(nameAndFolder: String) = project(nameAndFolder, nameAndFolder, new DefaultProject(_) with Defaults, common)
   
   lazy val firstClassFunctions = projectBasedOnCommon("first-class-functions")
   lazy val intro = projectBasedOnCommon("intro")
   lazy val ooTraits = projectBasedOnCommon("oo-traits")
   lazy val patternMatching = projectBasedOnCommon("pattern-matching")
-
 }
